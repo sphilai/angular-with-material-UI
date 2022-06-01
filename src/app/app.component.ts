c55f2097b6059936b8f70f6b08ae0de2a7672b14
sasipa_dev <sasipa.philai@gmail.com>
2022-06-01 22:04:27 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,Inject,ViewChild,AfterViewInit } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTable, MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,78 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-scss';
+  displayedColumns: string[] = ['productName', 'category', 'date', 'freshness','price','comment','action'];
+  dataSource !: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator !: MatPaginator;
+  @ViewChild(MatSort) sort !: MatSort;
+
+  constructor(private dialog: MatDialog,
+    private api : ApiService){
+
+  }
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+     width:'30%'
+    }).afterClosed().subscribe(val=>{
+      if(val==='save'){
+        this.getAllProducts();
+      }
+    })
+  }
+
+  getAllProducts(){
+    this.api.getProduct().subscribe({
+      next:(res)=>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error:(error)=>{
+        alert('error while get data')
+      }
+    })
+
+  }
+
+  deleteProduct(id:number){
+    this.api.deleteProduct(id).subscribe({
+      next:(res)=>{
+        alert("Product Deleted Successfully")
+        this.getAllProducts();
+      },
+      error:(error)=>{
+        alert('error while deleting')
+      }
+      
+    })
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  editProduct(row: any){
+    this.dialog.open(DialogComponent,{
+      width: '30%',
+      data:row
+    }).afterClosed().subscribe(val=>{
+      if(val==='updated'){
+        this.getAllProducts();
+      }
+    })
+  }
 }
+
+
